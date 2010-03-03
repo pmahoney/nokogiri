6ae427024c88b78c552ae84fc6ce2e2a75361628
Patrick Mahoney <pat@polycrystal.org>
2010-03-03 12:25:27 -0700

    Check if we are using libxml before checking libxml version.

--- test/html/test_document_fragment.rb
+++ test/html/test_document_fragment.rb
@@ -83,7 +83,8 @@ module Nokogiri
       def test_html_fragment_has_outer_text
         doc = "a<div>b</div>c"
         fragment = Nokogiri::HTML::Document.new.fragment(doc)
-        if Nokogiri::VERSION_INFO['libxml']['loaded'] <= "2.6.16"
+        if Nokogiri.uses_libxml? &&
+            Nokogiri::VERSION_INFO['libxml']['loaded'] <= "2.6.16"
           assert_equal "a<div>b</div><p>c</p>", fragment.to_s
         else
           assert_equal "a<div>b</div>c", fragment.to_s
@@ -123,7 +124,8 @@ module Nokogiri
       def test_to_xhtml
         doc = "<span>foo<br></span><span>bar</span>"
         fragment = Nokogiri::HTML::Document.new.fragment(doc)
-        if Nokogiri::VERSION_INFO['libxml']['loaded'] >= "2.7.0"
+        if Nokogiri.uses_libxml? &&
+            Nokogiri::VERSION_INFO['libxml']['loaded'] >= "2.7.0"
           assert_equal "<span>foo<br /></span><span>bar</span>", fragment.to_xhtml
         else
           assert_equal "<span>foo<br></span><span>bar</span>", fragment.to_xhtml
